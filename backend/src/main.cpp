25c1926704402fcb7ba9ae2634ce03c15444dc26
David Koerner <dkoerner@users.noreply.github.com>
2017-07-17 13:03:15 +0200

    removed some incorrect license information

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,28 +1,3 @@
-// ======================================================================== //
-// Copyright 2009-2016 Intel Corporation                                    //
-//                                                                          //
-// Licensed under the Apache License, Version 2.0 (the "License");          //
-// you may not use this file except in compliance with the License.         //
-// You may obtain a copy of the License at                                  //
-//                                                                          //
-//     http://www.apache.org/licenses/LICENSE-2.0                           //
-//                                                                          //
-// Unless required by applicable law or agreed to in writing, software      //
-// distributed under the License is distributed on an "AS IS" BASIS,        //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
-// See the License for the specific language governing permissions and      //
-// limitations under the License.                                           //
-// ======================================================================== //
-
-
-/* This is a small example tutorial how to use OSPRay in an application.
- *
- * On Linux build it in the build_directory with
- *   g++ ../apps/ospTutorial.cpp -I ../ospray/include -I .. -I ../ospray/embree/common ./libospray.so -Wl,-rpath,. -o ospTutorial
- * On Windows build it in the build_directory\$Configuration with
- *   cl ..\..\apps\ospTutorial.cpp /EHsc -I ..\..\ospray\include -I ..\.. -I ..\..\ospray\embree\common ospray.lib
- */
-
 #include <zmq.h>
 #include <czmq.h>
 #include <iostream>
